c3f322d798a49d721d5d2131a8d6b44180f627ef
Andrew E. Timmes <andrew.timmes@gmail.com>
2021-12-05 12:16:28 -0500

    add working 5b solution

--- 5/b/main.go
+++ 5/b/main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"log"
-	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -27,31 +26,23 @@ func main() {
 	seen := make(map[point]int)
 	for scanner.Scan() {
 		start, finish := parseLine(scanner.Text())
-		if start.x == finish.x {
-			i, j := enforceAscending(start.y, finish.y)
-			for p := newPoint(start.x, i); p.y <= j; p.y++ {
-				seen[p] += 1
-			}
-		} else if start.y == finish.y {
-			i, j := enforceAscending(start.x, finish.x)
-			for p := newPoint(i, start.y); p.x <= j; p.x++ {
-				seen[p] += 1
-			}
-		} else if math.Abs(float64(start.x)-float64(finish.x)) == math.Abs(float64(start.y)-float64(finish.y)) {
-			x_dir := 1
-			if start.x > finish.x {
-				x_dir = -1
-			}
-			y_dir := 1
-			if start.y > finish.y {
-				y_dir = -1
-			}
-			for p := newPoint(start.x, start.y); p.x != finish.x; p.x, p.y = p.x+x_dir, p.y+y_dir {
-				seen[p] += 1
-			}
-		} else {
-			panic("Weird line")
+		x_dir := 0
+		if start.x < finish.x {
+			x_dir = 1
+		} else if start.x > finish.x {
+			x_dir = -1
 		}
+		y_dir := 0
+		if start.y < finish.y {
+			y_dir = 1
+		} else if start.y > finish.y {
+			y_dir = -1
+		}
+		p := newPoint(start.x, start.y)
+		for ; p.x != finish.x || p.y != finish.y; p.x, p.y = p.x+x_dir, p.y+y_dir {
+			seen[p] += 1
+		}
+		seen[p] += 1
 	}
 	for k, v := range seen {
 		fmt.Println(strconv.Itoa(k.x), strconv.Itoa(k.y), v)
