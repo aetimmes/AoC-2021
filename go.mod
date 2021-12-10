84c7700804983e851e25d93c19d7698af013a6e6
Andrew E. Timmes <andrew.timmes@gmail.com>
2021-12-10 00:25:13 -0500

    checkpoint

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/aetimmes/AoC-2021
 
 go 1.18
 
-require github.com/aetimmes/go-set v0.0.3
+require github.com/aetimmes/go-set v0.0.3 // indirect
--- solutions/10a.go
+++ solutions/10a.go
@@ -8,18 +8,18 @@ import (
 	"strings"
 )
 
-var scores = map[string]int{
-	")": 3,
-	"]": 57,
-	"}": 1197,
-	">": 25137,
+var scores = map[byte]int{
+	')': 3,
+	']': 57,
+	'}': 1197,
+	'>': 25137,
 }
 
-var bracePairs = map[string]string{
-	")": "(",
-	"]": "[",
-	"}": "{",
-	">": "<",
+var bracePairs = map[byte]byte{
+	')': '(',
+	']': '[',
+	'}': '{',
+	'>': '<',
 }
 
 func F10a(filename string) {
@@ -41,5 +41,18 @@ func F10a(filename string) {
 }
 
 func scoreLine(s string) int {
-
+	q := make([]byte, 0, 100)
+	for i := range s {
+		_, ok := scores[s[i]]
+		if ok {
+			if q[len(q)-1] == bracePairs[s[i]] {
+				q = q[:len(q)-1]
+			} else {
+				return scores[s[i]]
+			}
+		} else {
+			q = append(q, s[i])
+		}
+	}
+	return 0
 }
